255903868502d780b56dee0d9ad2267ef5a57b78
Lurz0205 <91960598+Lurz0205@users.noreply.github.com>
2026-07-10 11:05:10 +0700

    fix

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,7 +2,7 @@ import { Client, ActivityType } from 'discord.js';
 import { logger } from '../utils/logger.js';
 
 export function registerReadyEvent(client: Client) {
-  client.once('ready', (readyClient) => {
+  client.once('clientReady', (readyClient) => {
     logger.success(`Bot is online! Logged in as ${readyClient.user.tag}`, 'ready');
     
     // Set custom activity presence
